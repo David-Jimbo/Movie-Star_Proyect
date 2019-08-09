92235b7e60d53bf57ac2a0753b95ab37b6db4d1b
Deivithy <rudi.jimbo@unl.edu.ec>
2019-08-09 18:20:11 -0500

    Configuracion de las dependencias

--- app.js
+++ app.js
@@ -3,21 +3,51 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require("hbs");
+var exphbs = require("express-handlebars");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var flash = require("connect-flash");
+var passport = require('passport');
+
 var app = express();
+//for BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", "views");
+app.engine(
+  "hbs",
+  exphbs({
+    extname: ".hbs",
+    defaultLayout: "",
+    layoutsDir: "",
+    partialsDir: ["views"]
+  })
+);
+app.set("view engine", "hbs");
 
+app.use(
+  session({
+    secret: "cuarto-sistemas-A",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+require("dotenv").config();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
