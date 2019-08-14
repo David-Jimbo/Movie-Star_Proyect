f46ea92e6e1d3899113a2b3c74a189d459a1136b
David-Jimbo <rudi.jimbo@unl.edu.ec>
2019-08-13 22:56:38 -0500

    Creacion de fragmentos para ver y registar peliculas

--- routes/index.js
+++ routes/index.js
@@ -11,28 +11,30 @@ var persona = new personaC();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Rol.crear_roles();
-  res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/principal'});
+  res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/principal', msg: { error: req.flash('error'), ok: req.flash('info')} });
 });
 
-router.post('/registrarse', function (req, res, next) {
+router.get('/registrarse', function (req, res, next) {
   res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Persona/registrar' });
 });
 
-router.get('/miPerfil', function (req, res, next) {
+router.get('/mi_perfil', function (req, res, next) {
   res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Persona/verPerfil' });
 });
 
-router.get('/editarPerfil', function (req, res, next) {
+router.get('/editar_perfil', function (req, res, next) {
   res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Persona/editarPerfil' });
 });
 
-router.get('/peliculas/nueva', function (req, res, next) {
-  res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Persona/registrar' });
+router.get('/peliculas', function (req, res, next) {
+  res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Peliculas/verTodas' });
 });
 
-router.get('/peliculas/verTodas', function (req, res, next) {
-  res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Persona/registrar' });
+router.get('/peliculas/nueva', function (req, res, next) {
+  res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Peliculas/registrarP' });
 });
+
+
 router.get('/test', function (req, res, next) {
   res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Persona/verPerfil' });
 });
