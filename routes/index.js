69ec06a82bf6235d64341e36f9e6dc2aef9725fc
David-Jimbo <rudi.jimbo@unl.edu.ec>
2019-08-18 13:13:32 -0500

    Validar peliculas y botones agregados

--- routes/index.js
+++ routes/index.js
@@ -9,34 +9,34 @@ var personaC = require('../controladores/persona_controller');
 var persona = new personaC();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  Rol.crear_roles();
-  res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/principal', msg: { error: req.flash('error'), ok: req.flash('info')} });
+router.get('/', function (req, res, next) {
+    Rol.crear_roles();
+    res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/principal', msg: { error: req.flash('error'), ok: req.flash('info') } });
 });
 
 router.get('/registrarse', function (req, res, next) {
-  res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Persona/registrar' });
+    res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Persona/registrar' });
 });
 
 router.get('/mi_perfil', function (req, res, next) {
-  res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Persona/verPerfil' });
+    res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Persona/verPerfil' });
 });
 
 router.get('/editar_perfil', function (req, res, next) {
-  res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Persona/editarPerfil' });
+    res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Persona/editarPerfil' });
 });
 
 router.get('/peliculas', function (req, res, next) {
-  res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Peliculas/verTodas' });
+    res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Peliculas/verTodas' });
 });
 
 router.get('/peliculas/nueva', function (req, res, next) {
-  res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Peliculas/registrarP' });
+    res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Peliculas/registrarP' });
 });
 
 
 router.get('/test', function (req, res, next) {
-  res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Persona/verPerfil' });
+    res.render('index', { title: 'Movie Star', fragmento: 'Fragmentos/Persona/verPerfil' });
 });
 
 
