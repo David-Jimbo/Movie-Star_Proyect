8f1593757b3c92833702b5344f3fabf9fe0853f3
EALeon16 <eduardo.leon@unl.edu.ec>
2019-08-19 10:57:46 -0500

    boorar metodos que no se ocupan

--- controladores/pelicula_controller.js
+++ controladores/pelicula_controller.js
@@ -34,62 +34,7 @@ class pelicula_controller {
 
     
 
-    guardar_p(req, res) {
-        if (req.body.external === "0") {
-            Pelicula.findAll({ where: { nombre_peli: req.body.nombre_peli } }).then(function (pelicula) {
-                if (pelicula) {
-                    req.flash('error', 'La pelicula ya ha sido registrada');
-                    res.redirect('/admin/peliculas/nueva');
-                } else {
-                    Pelicula.create({
-                        nombre_peli: req.body.nombre_peli,
-                        genero: DataTypes.req.body.genero,
-                        fecha_lanzamiento: req.body.fecha,
-                        sinopsis: req.body.sinopsis,
-                        clasificacion: req.body.clasificacion,
-                        duracion: req.body.duracion,
-                        director: req.body.director,
-                        protagonistas: req.body.protagonistas,
-                        estado: req.body.proyeccion,
-                        external_id: uuid.v4()
-                    }).then(function (newPelicula, created) {
-                        if (newPelicula) {
-                            //req.flash('ok', 'se ha guardado la pelicula');
-                            //res.redirect('/peliculas');
-                            res.send(newPelicula);
-
-                        }
-                    }).error(function (error) {
-                        res.send(error);
-                    });
-                }
-
-            }).catch(function (err) {
-                req.flash('error', 'Hubo un problema');
-                res.redirect('/peliculas');
-            });
-        } else {
-            Pelicula.update({
-                        nombre_peli: req.body.nombre_peli,
-                        genero: DataTypes.req.body.genero,
-                        fecha_lanzamiento: req.body.fecha,
-                        sinopsis: req.body.sinopsis,
-                        clasificacion: req.body.clasificacion,
-                        duracion: req.body.duracion,
-                        director: req.body.director,
-                        protagonistas: req.body.protagonistas,
-
-            }, { where: { external_id: req.body.external } }).then(function (updatePelicula, created) {
-                if (updatePelicula) {
-                    req.flash('ok', 'se ha modificado la pelicula');
-                    res.redirect('/peliculas');
-                }
-            }).error(function (error) {
-                res.send(error);
-            });
-        }
-
-    }
+ 
 
     guardar_pelicula(req, res){
         Pelicula.findAll({ where: { nombre_peli: req.body.nombre_peli } }).then(function (resultado){
